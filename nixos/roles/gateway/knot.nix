152acf11106fd123dda1161139fb854270b0907e
Ingolf Wagner <contact@ingolf-wagner.de>
2023-10-26 01:55:56 +0200

    now DNS works

--- nixos/roles/gateway/knot.nix
+++ nixos/roles/gateway/knot.nix
@@ -1,63 +1,21 @@
-{ pkgs, lib, dns, system, machines, toplevelDomain, ... }:
+{ pkgs, lib, system, machine, machines, toplevelDomain, ... }:
 with lib;
-with dns.lib.combinators;
-let
-  util = dns.util.${pkgs.system};
-  dnsConfigurationRaw = ''
-    $TTL 60
-
-    @ IN SOA cluster. ns1.cluster. (2023102500 86400 600 864000 60)
-    @ IN NS ns1
-
-    ns1 IN A 10.0.0.2
-
-    cor-net-gateway IN A 10.0.0.2
-    *.cor-net-gateway IN A 10.0.0.2
-    dev-cache IN A 10.0.0.3
-    *.dev-cache IN A 10.0.0.3
-    dev-consul-0 IN A 10.0.0.9
-    *.dev-consul-0 IN A 10.0.0.9
-    dev-monitor IN A 10.0.0.4
-    *.dev-monitor IN A 10.0.0.4
-    dev-nomad-0 IN A 10.0.0.5
-    *.dev-nomad-0 IN A 10.0.0.5
-    dev-nomad-1 IN A 10.0.0.7
-    *.dev-nomad-1 IN A 10.0.0.7
-    dev-nomad-2 IN A 10.0.0.8
-    *.dev-nomad-2 IN A 10.0.0.8
-    dev-vault-0 IN A 10.0.0.6
-    *.dev-vault-0 IN A 10.0.0.6
-
-  '';
-  dnsConfiguration = with dns.lib.combinators; {
-
-    NS = [ "ns.${toplevelDomain}" ];
-
-    SOA = {
-      nameServer = "ns.${toplevelDomain}.";
-      adminEmail = "admin@yolo.${toplevelDomain}";
-      serial = 2023102500;
-    };
-
-    subdomains = { ns.A = [ "10.0.0.2" ]; } //
-      (builtins.listToAttrs (map
-        ({ private_ipv4, name, ... }: {
-          inherit name;
-          value = {
-            A = [ private_ipv4 ];
-            subdomains."*".A = [ private_ipv4 ];
-          };
-        })
-        machines
-      ));
-  };
-
-in
-
 {
 
-  #networking.firewall.allowedTCPPorts = [ 52 ];
-  #networking.firewall.allowedUDPPorts = [ 52 ];
+  # putting the zone configuration in /etc/zones/ makes it easier to debug
+  environment.etc."zones/${toplevelDomain}.zone".text = ''
+    $TTL 60
+
+    @ IN SOA ${toplevelDomain}. ns1.${toplevelDomain}. (2023102500 86400 600 864000 60)
+    @ IN NS ns1
+
+    ns1 IN A ${machine.private_ipv4}
+
+    ${concatStringsSep "\n" (map ({private_ipv4, name, ...}: ''
+    ${name} IN A ${private_ipv4}
+    *.${name} IN A ${private_ipv4}
+    '') machines)}
+  '';
 
   services.knot = {
     enable = true;
@@ -72,7 +30,7 @@ in
 
       zone:
         - domain: ${toplevelDomain}
-          file: ${pkgs.writeText "cluster.zone" dnsConfigurationRaw}
+          file: /etc/zones/${toplevelDomain}.zone
     '';
 
   };
--- nixos/roles/gateway/unbound.nix
+++ nixos/roles/gateway/unbound.nix
@@ -2,19 +2,6 @@
 with lib;
 {
 
-  imports = [
-    # fixme: ugly hack for now this should be handled by knot
-    #{
-    #  services.unbound.settings.server = {
-    #    local-zone = [ ''"nomad" static'' ];
-    #    local-data = [
-    #      ''"traefik.nomad IN A 10.0.0.5"''
-    #      ''"app.nomad IN A 10.0.0.5"''
-    #    ];
-    #  };
-    #}
-  ];
-
   # not needed for sshuttle --dns at the moment
   networking.firewall.allowedTCPPorts = [ 53 ];
   networking.firewall.allowedUDPPorts = [ 53 ];
@@ -27,12 +14,12 @@ with lib;
         port = 53;
 
         use-syslog = true;
-        verbosity = 2;
-        #log-queries = true;
-        #log-servfail = true;
-        #log-replies = true;
+        verbosity = 3;
+        log-queries = true;
+        log-servfail = true;
+        log-replies = true;
 
-        # Mic92 empfehlung (copy-paste)
+        # Mic92 recommends (copy-paste)
         prefetch = "yes";
         prefetch-key = true;
         qname-minimisation = true;
@@ -40,38 +27,17 @@ with lib;
         # Breaks my email setup. Better rely on tls for security.
         val-permissive-mode = "yes";
 
+        # todo : make this part configurable from the machine
         access-control = [ "10.0.0.0/8 allow" ];
         interface = [ "0.0.0.0" ];
-
-        #domain-insecure = ''"${toplevelDomain}."'';
-        #domain-insecure = ''"${toplevelDomain}"'';
-        #local-zone = ''"${toplevelDomain}." transparent'';
-        #local-zone = ''"${toplevelDomain}" transparent'';
 
         local-zone = ''"${toplevelDomain}." nodefault'';
         domain-insecure = ''"${toplevelDomain}."'';
 
-        # cluster zone
-        # todo : wildcard does not work we need knot for that
-        #local-zone = [ ''"${toplevelDomain}" static'' ];
-        #local-data = map
-        #  ({ name, private_ipv4, ... }: ''"${name}.${toplevelDomain} IN A ${private_ipv4}"'')
-        #  machines;
-
       };
-
-      # forward all unkown to google
-      #forward-zone = [{
-      #  name = ''"${toplevelDomain}."'';
-      #  #forward-addr = "127.0.0.1@53";
-      #  forward-addr = "10.0.0.2@53";
-      #  forward-first = true;
-      #  forward-no-cache = true;
-      #}];
 
       stub-zone = {
         name = "${toplevelDomain}.";
-        #stub-addr = "127.0.0.1";
         stub-addr = "10.0.0.2@52";
       };
 
