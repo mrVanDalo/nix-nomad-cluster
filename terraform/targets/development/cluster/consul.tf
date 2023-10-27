682312082cbdd22c6a534ca9f8480c78e2ac44fa
Ingolf Wagner <contact@ingolf-wagner.de>
2023-10-28 00:38:09 +0200

    network wip

--- terraform/targets/development/cluster/consul.tf
+++ terraform/targets/development/cluster/consul.tf
@@ -1,43 +1,19 @@
 locals {
   consul_server_count = 1
 }
-resource "hcloud_server" "consul" {
-  count       = local.consul_server_count
-  name        = "${local.environment_short}-consul-${count.index}"
-  image       = "debian-11"
-  server_type = "cpx11"
-  ssh_keys    = [var.main_key]
-  network {
-    network_id = var.main_network
-  }
-  labels = {
-    role        = "consul"
-    environment = var.environment
-    cost        = 5.18 # cpx11
-  }
-  public_net {
-    ipv4_enabled = false
-    ipv6_enabled = false
-  }
-  user_data = <<EOH
-#cloud-config
-runcmd:
-  - ip route add default via ${var.default_gateway}
-  - echo nameserver 8.8.8.8 > /etc/resolv.conf
-EOH
-  lifecycle {
-    ignore_changes = [network]
-  }
-}
 
-module "consul_host_file" {
-  count            = local.consul_server_count
-  source           = "../../../modules/host_file"
-  host             = hcloud_server.consul[count.index]
-  to_relative_path = local.path_relative_to_include
-  to_repo_path     = local.get_path_to_repo_root
-  volumes          = []
-  default_gateway  = var.default_gateway
-  cidr             = var.cidr
-  nameserver       = var.nameserver
+module "consul" {
+  count                 = local.consul_server_count
+  source                = "../../../modules/hetzner_server"
+  name                  = "${local.environment_short}-consul-${count.index}"
+  cost                  = 5.18
+  server_type           = "cpx11"
+  default_gateway       = var.default_gateway
+  environment           = var.environment
+  nameserver            = var.nameserver
+  network_id            = var.main_network
+  role                  = "cache"
+  ssh_keys              = [var.main_key]
+  get_path_to_repo_root = local.get_path_to_repo_root
+  to_relative_path      = local.path_relative_to_include
 }
--- terraform/targets/development/cluster/nomad.tf
+++ terraform/targets/development/cluster/nomad.tf
@@ -1,43 +1,19 @@
 locals {
   nomad_server_count = 3
 }
-resource "hcloud_server" "nomad" {
-  count       = local.nomad_server_count
-  name        = "${local.environment_short}-nomad-${count.index}"
-  image       = "debian-11"
-  server_type = "cpx11"
-  ssh_keys    = [var.main_key]
-  network {
-    network_id = var.main_network
-  }
-  labels = {
-    role        = "nomad"
-    environment = var.environment
-    cost        = 5.18 # cpx11
-  }
-  public_net {
-    ipv4_enabled = false
-    ipv6_enabled = false
-  }
-  user_data = <<EOH
-#cloud-config
-runcmd:
-  - ip route add default via ${var.default_gateway}
-  - echo nameserver 8.8.8.8 > /etc/resolv.conf
-EOH
-  lifecycle {
-    ignore_changes = [network]
-  }
-}
 
-module "nomad_host_file" {
-  count            = local.nomad_server_count
-  source           = "../../../modules/host_file"
-  host             = hcloud_server.nomad[count.index]
-  to_relative_path = local.path_relative_to_include
-  to_repo_path     = local.get_path_to_repo_root
-  volumes          = []
-  default_gateway  = var.default_gateway
-  cidr             = var.cidr
-  nameserver       = var.nameserver
+module "nomad" {
+  count                 = local.nomad_server_count
+  source                = "../../../modules/hetzner_server"
+  name                  = "${local.environment_short}-nomad-${count.index}"
+  cost                  = 5.18
+  server_type           = "cpx11"
+  default_gateway       = var.default_gateway
+  environment           = var.environment
+  nameserver            = var.nameserver
+  network_id            = var.main_network
+  role                  = "cache"
+  ssh_keys              = [var.main_key]
+  get_path_to_repo_root = local.get_path_to_repo_root
+  to_relative_path      = local.path_relative_to_include
 }
